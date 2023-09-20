a8ab138599b27a697dfdcdbac667c0c6c6114921
Blue Teeth <daominhtri1000@gmail.com>
2023-09-20 11:04:53 +0700

    Init project

---
+++ CarRentingManagement/DataAccess/Models/Manufacturer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Models
+{
+    public partial class Manufacturer
+    {
+        public Manufacturer()
+        {
+            CarInformations = new HashSet<CarInformation>();
+        }
+
+        public int ManufacturerId { get; set; }
+        public string ManufacturerName { get; set; } = null!;
+        public string? Description { get; set; }
+        public string? ManufacturerCountry { get; set; }
+
+        public virtual ICollection<CarInformation> CarInformations { get; set; }
+    }
+}
